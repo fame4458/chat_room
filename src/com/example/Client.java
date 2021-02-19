92e3091e692d551f49c8cbfd68744647c793e47c
unknown <thinnapat.2542@mail.kmutt.ac.th>
2021-02-19 15:05:39 +0700

    echo all client

--- src/com/example/Client.java
+++ src/com/example/Client.java
@@ -11,11 +11,12 @@ public class Client {
         clientSocket.connect(new InetSocketAddress("127.0.0.1", 8080));
         System.out.printf("Connected from port %d\n", clientSocket.getLocalPort());
 
+        while (true) {
+
         Scanner userInput = new Scanner(System.in);
 
         Scanner scanner = new Scanner(clientSocket.getInputStream());
 
-        for (int i = 0; i < 10; i++) {
             System.out.print("Enter Command: ");
             String command = userInput.nextLine();
             clientSocket.getOutputStream().write((command + "\n").getBytes());
@@ -24,6 +25,5 @@ public class Client {
             System.out.println("Message : "+text);
             Thread.sleep(1000);
         }
-        clientSocket.close();
     }
 }
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,19 +3,22 @@ package com.example;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        ArrayList<Socket> clients = new ArrayList<>();
         ServerSocket serverSocket = new ServerSocket();
         serverSocket.bind(new InetSocketAddress(8080));
         System.out.println("Listening to 8080");
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.printf("Client connected %s:%d\n", clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
-            ClientHandler client = new ClientHandler( clientSocket);
+            ClientHandler client = new ClientHandler( clientSocket, clients);
+
             new Thread(client).start();
+            clients.add(clientSocket);
         }
     }
 
@@ -24,35 +27,27 @@ public class Main {
 
 class ClientHandler extends Main implements Runnable {
     private Socket socket;
+    private ArrayList<Socket> clients;
 
-
-    public ClientHandler ( Socket socket) {
+    public ClientHandler ( Socket socket, ArrayList<Socket> clients) {
         this.socket = socket;
+        this.clients = clients;
     }
-
 
     @Override
     public void run() {
         try {
-            Scanner scanner = new Scanner(socket.getInputStream());
-            while (scanner.hasNextLine()) {
+            while (true) {
+                Scanner scanner = new Scanner(socket.getInputStream());
                 String command = scanner.nextLine();
                 System.out.printf("GOT: %s\n", command);
-                sendMessage(command);
+                for (Socket client: clients){
+                    client.getOutputStream().write((command + "\n").getBytes());
+                    client.getOutputStream().flush();
+                }
             }
         } catch (Exception e) {
             // do nothing
         }
     }
-
-    public void sendMessage(String message){
-        try{
-            socket.getOutputStream().write((message + "\n").getBytes());
-            socket.getOutputStream().flush();
-        }catch (Exception e) {
-            //do nothing
-        }
-    }
-
-
 }
